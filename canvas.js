b6874cecf814023df986e83c59f8505e10c584d2
James Bull <james.s.bull@gmail.com>
2012-08-05 19:52:05 +0100

    A row of rectangles

--- canvas.js
+++ canvas.js
@@ -1,6 +1,50 @@
 var jim = {};
-jim.test = function () {
-	var canvas = $('#mainCanvas')[0].getContext('2d');
-	canvas.fillStyle="#FF0000";
-	canvas.fillRect(0,0,150,75);
+jim.rectangle = {
+	create: function (x, y, width, height, colour) {
+		return {
+			x: x,
+			y: y,
+			width: width,
+			height:height,
+			colour: colour,
+			draw: function (canvas) {
+				canvas.fillStyle = colour;
+				canvas.fillRect(x, y, width, height);
+			}
+		}
+	}
+}
+jim.list = {
+	range: function (start, length, increment) {
+		var nums = []
+		for(var i = 0 ; i < length ; i++) {
+			nums.push(start+ (i*increment));
+		}
+		return nums;
+	}
+}
+jim.checkedPattern = function (rows, firstColour, secondColour) {
+
+	var canvas, element, currentX;
+	element = $('#mainCanvas')[0]
+	canvas = element.getContext('2d');
+
+	
+	var currentColour = function (row) {
+		return row % 2 === 0 ? firstColour : secondColour
+	}
+		
+	var draw = function () {
+		var checkWidth = element.width / rows,
+			startingPoints = jim.list.range(0, rows, checkWidth);
+		
+		$.each(startingPoints, function (i, startingPoint) {
+			jim.rectangle.create(startingPoint, 0, checkWidth, 100, currentColour(i)).draw(canvas);
+		});	
+	}
+	
+	return {
+		draw : draw
+	}
+
 }
