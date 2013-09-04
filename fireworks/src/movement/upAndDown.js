561faba11cb8f462e502c65b767885214fc193fe
James Bull <james.s.bull@gmail.com>
2013-09-04 23:42:22 +0100

    Added some objects to calculate velocity and speed

--- fireworks/src/movement/upAndDown.js
+++ fireworks/src/movement/upAndDown.js
@@ -1,7 +1,32 @@
 namespace("jim.movement.upAndDown");
+namespace("jim.movement.acceleration");
+namespace("jim.movement.velocity");
+namespace("jim.movement.distance");
 
-jim.movement.upAndDown.create = function (x,y) {
+jim.movement.upAndDown.create = function (point) {
     return {
-        nextPosition: function () {return jim.point.create(x,y)}
+        nextPosition: function () {return point}
     };
 }
+
+jim.movement.acceleration.create = function(force, mass) {
+    return {
+        value: function () {
+            return force / mass;
+        }
+    }
+};
+jim.movement.velocity.create = function(u, a, t) {
+    return {
+        value: function () {
+            return u + a*t;
+        }
+    }
+};
+jim.movement.distance.create = function(u, a, t) {
+    return {
+        value: function () {
+            return (u * t) + (a/2)*t*t;
+        }
+    }
+};
--- fireworks/src/test/specs/movementSpec.js
+++ fireworks/src/test/specs/movementSpec.js
@@ -1,10 +1,44 @@
-describe("A linear colour gradient", function() {
+describe("Physics and movement: ", function() {
+
+    // A movement needs to take whatas the initial args?
+    // Needs a starting position it also needs a force
+    // How do I get to initial velocity from a force?
+    // F=ma  V=u+at
+    // F is initial force
+    // m I decide too
+    // So I can calculate a; f=ma; a=f/m;
+    // Then v=u+at
+    // I want to know v; u = 0; a = f/m; t should be a sixtieth of a second;
+    // can I find out how long a frame takes in js or find out how much time passed?
+    // I can find out how mch time passed. I get a timestamp passed to my callback so can be worked out
+
 
     it("the first location given should be the starting location", function() {
-        var start = jim.movement.upAndDown.create(0,0);
+        var start = jim.movement.upAndDown.create(jim.point.create(0,0));
         var result = start.nextPosition();
         expect(result.x).toBe(0);
         expect(result.y).toBe(0);
+    });
+
+    it("acceleration is calculated from force and mass", function() {
+        var accel = jim.movement.acceleration.create(4, 2);
+        expect(accel.value()).toBe(2);
+
+    });
+
+    it("final velocity is initial velocity plus the product of acceleration and time", function() {
+        var spid =  jim.movement.velocity.create;
+        expect(spid(0, 2, 5).value()).toBe(10);
+        expect(spid(10, 1, 10).value()).toBe(20);
+        expect(spid(32, -2, 5).value()).toBe(22);
+    });
+
+    it("distance is ut+1/2at^2 ", function() {
+        var spid =  jim.movement.distance.create;
+        expect(spid(0, 2, 5).value()).toBe(25);
+        expect(spid(2, 4, 10).value()).toBe(220);
+        expect(spid(10, 1, 10).value()).toBe(150);
+        expect(spid(30, -2, 5).value()).toBe(125);
     });
 
 });
